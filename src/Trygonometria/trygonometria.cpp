aeac2c6246d485db2fa639925f79b4dc9bd200cb
agatmom326 <58747143+agatmom326@users.noreply.github.com>
2022-03-17 13:07:27 +0100

    Sterowanie procesem kompilacji

--- src/Trygonometria/trygonometria.cpp
+++ src/Trygonometria/trygonometria.cpp
@@ -1,28 +1,32 @@
 #include "trygonometria.h" 
 #include <cmath> 
 
+#define _USE_MATH_DEFINES 
+
+const double pi = 3.1415926535897932384626433832795;
+
 double degreemath::sinus(double degree) {
  //zamiana stopni na radiany: pi radianów = 180 stopni 
-double radian = degree/180;
+double radian = degree*pi/180;
 
 return std::sin(radian);  //funkcja biblioteczna z piku <cmath> 
 } 
 
 double degreemath::cosinus(double degree) {
  //zamiana stopni na radiany: pi radianów = 180 stopni 
-    double radian = degree/180;
+    double radian = degree*pi/180;
 return std::cos(radian);  //funkcja biblioteczna z piku <cmath> 
 } 
 
 double degreemath::tangens(double degree) {
  //zamiana stopni na radiany: pi radianów = 180 stopni 
-    double radian = degree/180;
+    double radian = degree*pi/180;
 return std::tan(radian);  //funkcja biblioteczna z piku <cmath> 
 } 
 
 double degreemath::cotangens(double degree) {
  //zamiana stopni na radiany: pi radianów = 180 stopni 
-    double radian = degree/180;
+    double radian = degree*pi/180;
 
 return std::tan(radian);  //funkcja biblioteczna z piku <cmath> 
 } 
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
-#include "trygonometria.h"
-int main() { 
-	
-	double wynikSin = degreemath::sinus(180);
-	double wynikTg = degreemath::cosinus(180);
 
-	cout << wynikTg;
+#include "LaborkaConfig.h"
+
+#ifdef USE_TRIGONOMETRY_DEGREE 
+	#include "trygonometria.h"
+#else 
+	#include<cmath>
+#endif
+
+int main() { 
+
+const double pi = 3.1415926535897932384626433832795;
+
+//double wynikSin = 0;
+#ifdef USE_TRIGONOMETRY_DEGREE 
+	double wynikSin = degreemath::sinus(90); //k¹t w stopniach
+	cout << wynikSin;
+#else 
+	double wynikSin = sin(pi/2); //k¹t w radianach
+	cout << wynikSin;
+#endif
+	//double wynikSin = degreemath::sinus(180);
+	//double wynikTg = degreemath::cosinus(180);
+	
 	return 0;
 
 
